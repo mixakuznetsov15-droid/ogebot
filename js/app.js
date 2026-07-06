735e425ac172537b636cb05224210c69957c6e52
mixakuznetsov15-droid <mixakuznetsov15@gmail.com>
2026-07-06 08:37:12 +0300

    Update app.js

--- js/app.js
+++ js/app.js
@@ -32,3 +32,30 @@ window.nextQ = nextQ;
 window.shareBossResult = shareBossResult;
 window.closeRewardModal = closeRewardModal;
 window.closeProfessorModal = closeProfessorModal;
+// Запуск профессора Гео
+var professor = new ProfessorSystem(PROFESSOR_MESSAGES, {
+  default: 'images/professor_default.png',
+  happy: 'images/professor_happy.png',
+  sad: 'images/professor_sad.png',
+  hint: 'images/professor_hint.png'
+});
+professor.init();
+professor.showGreeting();
+
+// Подсказка профессора (вызывается при нажатии кнопки в квизе)
+window.askProfessor = function() {
+  if (!window.quizState) window.quizState = {};
+  if (!window.quizState.hintLevel) window.quizState.hintLevel = 0;
+  window.quizState.hintLevel++;
+  var hint = professor.getHint(window.quizState.topicKey, window.quizState.hintLevel);
+  var explainBox = document.getElementById('ai-explain');
+  if (explainBox) {
+    explainBox.style.display = 'block';
+    explainBox.textContent = hint.text;
+  }
+};
+
+// Открыть чат с профессором (заглушка, можно потом развить)
+window.openAIChat = function() {
+  professor.showExplanation('Давай разберём это задание вместе!');
+};
